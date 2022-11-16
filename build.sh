cc4a9a0e901a0399861579bbd620392d91a3fe65
luthfan <luthfanlubis@gmail.com>
2022-11-17 02:10:20 +0900

    Removed old SoaAlloc example

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 /usr/local/cuda/bin/nvcc -DNDEBUG -std=c++11 nbody.cu rendering.cu -lSDL2 -o nbody -arch compute_61
 /usr/local/cuda/bin/nvcc -DNDEBUG -std=c++11 nbody_soa.cu -o nbody_soa -arch compute_61
-# /usr/local/cuda/bin/nvcc -DNDEBUG -std=c++11 nbody_soaalloc.cu rendering_soa.cu -I. -Ilib/soa-alloc -o nbody_soaalloc -arch compute_61 --expt-relaxed-constexpr -maxrregcount=64 -lSDL2
 /usr/local/cuda/bin/nvcc -Xcudafe "--diag_suppress=1427" "$@" -O3 -DNDEBUG -std=c++11 -lineinfo --expt-extended-lambda -gencode arch=compute_50,code=sm_50 -gencode arch=compute_61,code=sm_61 -maxrregcount=64 -Ilib/dynasoar -I. -Ilib/dynasoar/lib/cub nbody_dynasoar.cu rendering_dynasoar.cu -lSDL2 -o nbody_dynasoar
--- rendering_soa.h
+++
@@ -1,9 +0,0 @@
-#ifndef RENDERING_SOA__H
-#define RENDERING_SOA__H
-
-void init_renderer();
-void close_renderer();
-void draw(float* host_Body_pos_x, float* host_Body_pos_y,
-          float* host_Body_mass);
-
-#endif  // RENDERING_SOA__H
