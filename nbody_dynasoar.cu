f724394596c1ce111c6d3b235ffc28be2b022881
kyosu <sho013039@gmail.com>
2023-01-05 17:58:21 +0900

    fix

--- nbody_dynasoar.cu
+++ nbody_dynasoar.cu
@@ -29,8 +29,7 @@ __device__ void Body::apply_force(Body* other) {
   float dx = -(other->pos_x_ - pos_x_);
   float dy = -(other->pos_y_ - pos_y_);
   float r = sqrt(dx * dx + dy * dy);
-  float force = kGravityConstant * mass_ * other->mass_ / (r * r);
-  force *= 0.1;
+  float force = kGravityConstant * mass_ * other->mass_ / (r * r + 0.05);
   other -> force_x_ += force * dx / r;
   other -> force_y_ += force * dy / r;
 }
